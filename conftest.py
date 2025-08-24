3d201a0b1aa5b8e78b54af64406393de626eb258
RMX <remix-92@mail.ru>
2025-08-24 22:19:32 +0300

    allure353

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
--- test_dynamic_labels.py
+++ test_dynamic_labels.py
@@ -5,12 +5,13 @@ from selene.support.shared.jquery_style import s
 import test_selene
 
 def test_issues_find():
-    # Динамические лейблы Allure
+
     allure.dynamic.feature("Issues в репозитории GitHub")
     allure.dynamic.story("Поиск задач в чужом публичном репозитории")
     allure.dynamic.severity(Severity.CRITICAL)
     allure.dynamic.tag("web", "github", "search")
     allure.dynamic.link("https://github.com/eroshenkoam/allure-example", name="Репозиторий examples")
+
 
 
     browser.open("https://github.com/home")
