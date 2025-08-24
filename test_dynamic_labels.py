1de5a911fe5d5dc73d4de2a79f4c567d7624df64
RMX <remix-92@mail.ru>
2025-08-24 03:39:06 +0300

    allure335

--- test_dynamic_labels.py
+++ test_dynamic_labels.py
@@ -2,14 +2,17 @@ import allure
 from allure_commons.types import Severity
 from selene import browser, by
 from selene.support.shared.jquery_style import s
+import tempfile
+import uuid
+
+# Создание уникальной временной директории
+temp_dir = f"/tmp/selenium_{uuid.uuid4().hex[:8]}"
+browser.config.driver_options.add_argument(f'--user-data-dir={temp_dir}')
+browser.config.driver_options.add_argument('--no-sandbox')
+browser.config.driver_options.add_argument('--disable-dev-shm-usage')
 
 
 def test_issues_find():
-    # Настройка браузера с уникальной директорией пользовательских данных
-    browser.config.driver_options.add_argument('--user-data-dir=/tmp/selenium_user_data')
-    browser.config.driver_options.add_argument('--no-sandbox')
-    browser.config.driver_options.add_argument('--disable-dev-shm-usage')
-
     # Динамические лейблы Allure
     allure.dynamic.feature("Issues в репозитории GitHub")
     allure.dynamic.story("Поиск задач в чужом публичном репозитории")
@@ -17,9 +20,11 @@ def test_issues_find():
     allure.dynamic.tag("web", "github", "search")
     allure.dynamic.link("https://github.com/eroshenkoam/allure-example", name="Репозиторий examples")
 
+    # Открытие страницы GitHub
     browser.open("https://github.com")
     browser.driver.maximize_window()
 
+    # Выполнение действий
     s(".header-search-button").click()
     s("input.FormControl-input.QueryBuilder-Input.FormControl-medium").type("eroshenkoam/allure-example").press_enter()
     s(by.link_text("eroshenkoam/allure-example")).click()
