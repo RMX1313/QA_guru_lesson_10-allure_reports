69bb4ab41ddf58fa6a19c681e9e90e638f2f4ce0
RMX <remix-92@mail.ru>
2025-08-24 03:48:49 +0300

    allure348

--- test_dynamic_labels.py
+++ test_dynamic_labels.py
@@ -1,23 +1,27 @@
 import allure
 from allure_commons.types import Severity
-from selene import browser, by
+from selene import browser, by, be
 from selene.support.shared.jquery_style import s
 import tempfile
-import uuid
 import pytest
+from selenium import webdriver
 
 
 # Фикстура для настройки браузера
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    # Создание уникальной временной директории для пользовательских данных
+    # Создание уникальной временной директории
     temp_dir = tempfile.mkdtemp(prefix='selenium_')
 
-    # Настройка опций драйвера
-    browser.config.driver_options.add_argument(f'--user-data-dir={temp_dir}')
-    browser.config.driver_options.add_argument('--no-sandbox')
-    browser.config.driver_options.add_argument('--disable-dev-shm-usage')
-    browser.config.driver_options.add_argument('--headless')  # Для CI/CD
+    # Инициализация опций драйвера
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'--user-data-dir={temp_dir}')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--headless')
+
+    # Установка опций в конфигурацию Selene
+    browser.config.driver_options = options
 
     yield  # Здесь выполняется тест
 
@@ -25,17 +29,18 @@ def setup_browser():
     browser.quit()
 
 
+@allure.feature("Issues в репозитории GitHub")
+@allure.story("Поиск задач в чужом публичном репозитории")
+@allure.severity(Severity.CRITICAL)
+@allure.tag("web", "github", "search")
+@allure.link("https://github.com/eroshenkoam/allure-example", name="Репозиторий examples")
 def test_issues_find():
-    # Динамические лейблы Allure
+    # Динамические лейблы (альтернатива декораторам)
     allure.dynamic.feature("Issues в репозитории GitHub")
     allure.dynamic.story("Поиск задач в чужом публичном репозитории")
-    allure.dynamic.severity(Severity.CRITICAL)
-    allure.dynamic.tag("web", "github", "search")
-    allure.dynamic.link("https://github.com/eroshenkoam/allure-example", name="Репозиторий examples")
 
     # Открытие страницы GitHub
     browser.open("https://github.com")
-    browser.driver.maximize_window()
 
     # Выполнение действий
     s(".header-search-button").click()
@@ -45,5 +50,3 @@ def test_issues_find():
 
     # Проверка результатов
     s(by.partial_text("#")).should(be.visible)
-
-
