c3557e0019af43e936508961b575219918c0f7ab
RMX <remix-92@mail.ru>
2025-08-24 03:35:32 +0300

    allure335

--- test_dynamic_labels.py
+++ test_dynamic_labels.py
@@ -2,9 +2,14 @@ import allure
 from allure_commons.types import Severity
 from selene import browser, by
 from selene.support.shared.jquery_style import s
-import test_selene
+
 
 def test_issues_find():
+    # Настройка браузера с уникальной директорией пользовательских данных
+    browser.config.driver_options.add_argument('--user-data-dir=/tmp/selenium_user_data')
+    browser.config.driver_options.add_argument('--no-sandbox')
+    browser.config.driver_options.add_argument('--disable-dev-shm-usage')
+
     # Динамические лейблы Allure
     allure.dynamic.feature("Issues в репозитории GitHub")
     allure.dynamic.story("Поиск задач в чужом публичном репозитории")
@@ -12,10 +17,13 @@ def test_issues_find():
     allure.dynamic.tag("web", "github", "search")
     allure.dynamic.link("https://github.com/eroshenkoam/allure-example", name="Репозиторий examples")
 
-
-    browser.open("https://github.com/home")
+    browser.open("https://github.com")
     browser.driver.maximize_window()
+
     s(".header-search-button").click()
     s("input.FormControl-input.QueryBuilder-Input.FormControl-medium").type("eroshenkoam/allure-example").press_enter()
-    s(by.link_text("eroshenkoam/allure-example-bamboo")).click()
+    s(by.link_text("eroshenkoam/allure-example")).click()
     s("#issues-tab").click()
+
+    # Закрытие браузера после теста
+    browser.quit()
