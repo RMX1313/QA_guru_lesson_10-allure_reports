2c4829e762784dc21df601ff029035ed45e226e7
RMX <remix-92@mail.ru>
2025-08-24 22:02:13 +0300

    allure353

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+
+    # Добавляем уникальный user-data-dir для каждого запуска
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome_profile")
+
+    browser.config.driver_options = chrome_options
+    browser.config.base_url = 'https://github.com'
+    browser.config.timeout = 15.0
+
+    yield
+
+    browser.quit()
